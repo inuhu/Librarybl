39c493974449df112e14a475a46d5a5bd0378684
inuhu <sokpro162@gmail.com>
2023-08-05 01:38:32 +0300

    Админка

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+
+# --------------------------
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+
+admin.site.register(Author, AuthorAdmin)
+
+# --------------------------
+admin.site.register(Genre)
+
+
+# --------------------------
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+
+# --------------------------
+admin.site.register(Language)
