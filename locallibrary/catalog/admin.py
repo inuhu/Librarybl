8fc9d4123ff179e8aea80f1011055bc4c9aaee26
inuhu <sokpro162@gmail.com>
2023-08-05 21:39:47 +0300

    Админка

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -2,15 +2,28 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    fields = ['book', 'status']
+
+
+class BooksInline(admin.TabularInline):
+    model = Book
+    fields = ['title', 'isbn']
+
+
+# --------------------------
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 
 # --------------------------
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-
+    fields = [('first_name', 'last_name'), ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
 
 admin.site.register(Author, AuthorAdmin)
 
@@ -22,7 +35,15 @@ admin.site.register(Genre)
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+    list_display =('id', 'book', 'due_back')
 
-
-# --------------------------
+    fieldsets = (
+        ('What the book', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+    # --------------------------
 admin.site.register(Language)
